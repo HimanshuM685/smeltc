b24603a2e2733f04bde4c4f1627118d8c0727424
Himanshu Malik <himanshumalik0069@gmail.com>
2025-05-26 11:02:43 +0530

    Added lexer function prototypes, fixed macros.inc

--- src/main.asm
+++ src/main.asm
@@ -1,6 +1,13 @@
-global _start
+%include "macros.inc"
+%include "lexer.inc"
 
-section .text
-_start:
-  call print_hello
-  call exit_program
+section .data
+  prompt        db 'shell> ', 0
+  prompt_len    equ $ - prompt - 1
+  newline       db 10, 0
+  input_buffer  times MAX_TEXT_LEN db 0
+
+  illegal_char_msg  db 'Illegal Character: ', 0
+  error_at_msg      db 'File', 0
+  line_msg          db ', line', 0
+  
